ca05a335baecd7858426eec974a79f07dd8901f8
Subrata Sarker <subrata_sarker@yahoo.com>
2026-01-03 19:31:47 -0500

    Use argparse to run data generator and ETL pipeline from the CLI

--- main.py
+++ main.py
@@ -1,5 +1,50 @@
+import argparse
 from etl.etl_pipeline import run_etl
+from scripts.generate_messy_excel_file import (
+    generate_data, 
+    inspect_generated_data
+)
 
 
-# Run the ETL Pipeline
-run_etl()
+def main():
+    parser = argparse.ArgumentParser(description="Sales Data Tools")
+    subparsers = parser.add_subparsers(dest="command")
+
+    # generate subcommand
+    gen = subparsers.add_parser("generate", help="Generate random sales data")
+    gen.add_argument(
+        "--rows", 
+        type=int,
+        default=10,
+        help="Number of rows")
+    gen.add_argument(
+        "--test",
+        action="store_true",
+        help="Test generated data in Excel file."
+    )
+
+    # etl subcommand
+    subparsers.add_parser("etl", help="Run ETL pipeline")
+
+    args = parser.parse_args()
+
+    if args.command == "generate":
+        if args.rows <= 0:
+            raise parser.error("rows must be a positive integer")
+        
+        df = generate_data(args.rows)
+        print(df.head(5))
+        print("==> Generation successful!\n")
+
+        if args.test:
+            print("==> Excel file ispection report:")
+            inspect_generated_data()
+    
+    elif args.command == "etl":
+        run_etl()
+    else:
+        parser.error("You must specify a command: generate [--rows] or etl")
+
+
+if __name__ == "__main__":
+    main()
