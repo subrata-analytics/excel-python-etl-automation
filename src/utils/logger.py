2aa6c8ebd49f22ba3a8f04c3ba1b1097bd718c32
Subrata Sarker <subrata_sarker@yahoo.com>
2026-01-07 16:06:51 -0500

    Add logger.py with basic configuration for logging

---
+++ src/utils/logger.py
@@ -0,0 +1,13 @@
+import logging
+
+
+def get_logger(name, level=logging.INFO):
+
+    logging.basicConfig(
+        level=level,
+        format="%(asctime)s | %(levelname)s | %(name)s | %(message)s"
+    )
+    
+    logger = logging.getLogger(name)
+
+    return logger
